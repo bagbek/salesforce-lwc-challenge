8725a548610f15240a3d9681752c6063d6eeb79b
Bayram <unfmerv@gmail.com>
2023-05-02 14:57:45 -0700

    import:object-fields
    Imported required an object and fields. Also, created constant variable columns.

--- lwc-challenge/force-app/main/default/lwc/accountList/accountList.js
+++ lwc-challenge/force-app/main/default/lwc/accountList/accountList.js
@@ -1,6 +1,23 @@
 import { LightningElement, wire } from 'lwc';
 import getAccounts from '@salesforce/apex/AccountController.getAccountList';
 
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+
+import NAME from '@salesforce/schema/Account.Name';
+import INDUSTRY from '@salesforce/schema/Account.Industry';
+import ANNUAL_REVENUE from '@salesforce/schema/Account.AnnualRevenue';
+import PHONE from '@salesforce/schema/Account.Phone';
+import WEBSITE from '@salesforce/schema/Account.Website';
+
+const COLUMNS = [
+    { label: 'Name', fieldName: NAME.fieldApiName, type: 'text', sortable: "true" },
+    { label: 'Industry', fieldName: INDUSTRY.fieldApiName, type: 'text', sortable: "true" },
+    { label: 'Annual Revenue', fieldName: ANNUAL_REVENUE.fieldApiName, type: 'currency', sortable: "true" },
+    { label: 'Phone', fieldName: PHONE.fieldApiName, type: 'text', sortable: "true" },
+    { label: 'Website', fieldName: WEBSITE.fieldApiName, type: 'text', sortable: "true" }
+];
+
+
 export default class AccountList extends LightningElement {
-    
+
 }
